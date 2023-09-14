11d0f2630f6078126acb3c527981a2b00ac07eb7
Mark Bradley <braddle@gmail.com>
2023-09-14 17:11:17 +0100

    Setting up for steaming

---
+++ src/main/java/com/armakuni/RomanNumeral.java
@@ -0,0 +1,8 @@
+package com.armakuni;
+
+public class RomanNumeral {
+
+    public boolean getTemp() {
+        return true;
+    }
+}
---
+++ src/test/java/com/armakuni/RomanNumeralTest.java
@@ -0,0 +1,14 @@
+package com.armakuni;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RomanNumeralTest {
+
+    @Test
+    void temp() {
+        RomanNumeral rn = new RomanNumeral();
+        assertTrue(rn.getTemp());
+    }
+}
